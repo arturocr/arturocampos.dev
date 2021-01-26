3c5ed4acf7a492d4406daf9f219ed665b7021bb0
Arturo Campos <arturo025@gmail.com>
2021-01-26 11:55:40 -0600

    feat: projects homepage

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import Heading from '@/components/heading';
@@ -9,11 +10,24 @@ const Calculator = () => {
   return (
     <>
       <Heading>{t('projects')}</Heading>
-      <ul className='my-4 list-disc list-inside'>
-        <li>
-          <Link href='/projects/tax-calculator-crc'>{t('calculator')}</Link>
-        </li>
-      </ul>
+      <section className='flex flex-wrap my-4'>
+        <article className='w-1/2 md:w-1/3'>
+          <Link href='/projects/tax-calculator-crc'>
+            <a className='block p-4 m-1 font-bold text-center transition-all border border-gray-200 rounded-md shadow-lg hover:shadow-xl'>
+              <Image
+                alt={t('calculator')}
+                className='rounded-md'
+                height={1134}
+                layout='responsive'
+                priority
+                src='/images/calculator-logo.png'
+                width={1134}
+              />
+              {t('calculator')}
+            </a>
+          </Link>
+        </article>
+      </section>
     </>
   );
 };
