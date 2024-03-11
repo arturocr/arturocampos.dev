6f3ef2d3089ab06cd8a845b6d0507d60e16e3fe2
Arturo Campos <inbox@arturocr.com>
2024-03-11 13:12:54 -0600

    fix: change twitter icon to x (#41)
    * fix: change twitter icon to x
    
    * fix: update svg

--- components/contact-links.js
+++ components/contact-links.js
@@ -3,21 +3,18 @@ const ContactLinks = () => (
     <li className='inline-flex mx-1'>
       <a
         className='inline-block p-2 transition-colors text-middle hover:text-secondary'
-        href='https://twitter.com/arturocr'
+        href='https://x.com/arturocr'
         rel='noopener noreferrer'
         target='_blank'
       >
         <svg
-          className='w-5 stroke-2'
-          fill='none'
-          stroke='currentColor'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          viewBox='0 0 24 24'
+          className='w-5'
+          fill='currentColor'
+          viewBox='0 0 512 512'
           xmlns='http://www.w3.org/2000/svg'
         >
-          <title>Twitter</title>
-          <path d='M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z' />
+          <title>x.com</title>
+          <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/>
         </svg>
       </a>
     </li>
