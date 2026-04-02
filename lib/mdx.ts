6b2f8b0c486c43038d5596c13f0de757a8f603d7
Arturo Campos <info@arturocampos.dev>
2026-04-01 23:05:22 -0600

    content: temporal api post

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -1,9 +1,10 @@
 import mdxPrism from 'mdx-prism';
 import remarkCodeTitles from 'remark-code-titles';
+import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 
 export const mdxOptions = {
-  remarkPlugins: [remarkCodeTitles] as any[],
+  remarkPlugins: [remarkCodeTitles, remarkGfm] as any[],
   rehypePlugins: [
     [
       rehypeRaw,
